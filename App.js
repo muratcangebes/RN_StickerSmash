8d76c6aa5228656a0e636164b02cbe2b84788403
Muratcan Gebeş <muratcan.gebes19@gmail.com>
2024-01-01 20:57:06 +0300

    StickerSmash commit

---
+++ App.js
@@ -0,0 +1,145 @@
+import { useState,useRef } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
+import EmojiPicker from './components/EmojiPicker';
+import EmojiList from './components/EmojiList';
+import EmojiSticker from './components/EmojiSticker';
+import * as MediaLibrary from 'expo-media-library';
+import { captureRef } from 'react-native-view-shot';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
+
+export default function App() {
+  
+  const imageRef = useRef();
+
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+
+  const [pickedEmoji, setPickedEmoji] = useState(null);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions,setShowAppOptions] = useState(false);
+
+  const [selectedImage,setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if(!result.canceled){
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    }else{
+      alert('Herhangi bir resim seçmediniz.');
+    }
+  };
+
+  if (status === null) {
+    requestPermission();
+  }
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+  
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    
+  };
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.imageContainer}>
+      <View ref={imageRef} collapsable={false}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
+        {pickedEmoji !==null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+        </View>
+      </View>
+      
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton style={styles.optionsText} icon="refresh" label="Reset" onPress={onReset}/>
+            <CircleButton onPress={onAddSticker} />
+            <IconButton style={styles.optionsText} icon="save-alt" label="Save" onPress={onSaveImageAsync}  />
+          </View>
+        </View>
+      ):(
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Bir fotoğraf seçin" onPress={pickImageAsync}/>
+        <Button label="Bu fotoğrafı kullanın" onPress={() => setShowAppOptions(true)} />
+      </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
+      <StatusBar style="auto" />
+    </GestureHandlerRootView>
+  );
+}
+
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerContainer:{
+    flex: 1 / 3,
+    alignItems: 'center',
+  },
+  imageContainer:{
+    flex: 1,
+    paddingTop: 58,
+  },
+  image:{
+    width: 320,
+    height:440,
+    borderRadius: 18,
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  optionsText:{
+    color:'#ffd33d',
+  },
+});
+
+
+
+//<Image source={PlaceholderImage} style={styles.image} />
